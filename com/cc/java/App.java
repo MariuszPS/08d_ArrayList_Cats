fb9f0d8b75f3c29b44531566f566616fc7d62d10
MariuszPS <k8kpol@gmail.com>
2022-04-27 12:44:23 +0200

    nameList/for-loop

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,7 +4,29 @@ public class App {
     
     public static void main(String[] args) {
      
+        // Vorüberlegung
+        // Cat cat = new Cat("Grizabella");
+        // Cat cat1 = new Cat("Grizabella");
+        // Cat cat2 = new Cat("Tiger");    
+        // Cat cat3 = new Cat("Alonso");
+        // Cat cat4 = new Cat("Suzanne");
+
+
+        //cat1 / cat2 .. more of the same .. DRY
+
+        String [] nameArr = {"Grizabella","Rum Tum Tugger","Alonzo","Suzanne"};
+
+        for (int i = 0; i < nameArr.length; i++) {
+            new Cat(nameArr[i]);  //Objekte, ABER keine Referenz
+        }
+
+
     }
+
+
+
+
+
 
     public static void output(String outputStr) {
         System.out.println(outputStr);
